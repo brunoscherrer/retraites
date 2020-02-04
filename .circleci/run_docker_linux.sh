ecb9b3970c5e0ccf04d62e54ca21f102e10d5662
Michaël BAUDIN <michael.baudin@gmail.com>
2020-02-04 16:06:46 +0100

    Ajoute la compilation TEX dans CI

--- .circleci/run_docker_linux.sh
+++ .circleci/run_docker_linux.sh
@@ -22,3 +22,15 @@ cd doc
 jupyter nbconvert --to notebook --execute Description-du-composant-retraites.ipynb
 cd ..
 
+# Doc Latex 1
+cd doc
+pdflatex notice.tex
+pdflatex notice.tex
+cd ..
+
+# Doc Latex 2
+cd doc/Article4
+pdflatex article4-analyse-impact.tex
+pdflatex article4-analyse-impact.tex
+cd ..
+
