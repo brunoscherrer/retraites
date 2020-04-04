f41cf7430c9545b6a89cac7529a8ecfb687eb063
Michaël BAUDIN <michael.baudin@gmail.com>
2020-04-05 00:40:46 +0200

    Corrige les dépendances

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='retraites',
-    keywords=("graphics"),
+    keywords=("COR", "pension"),
     version='0.1.1',
     packages=find_packages(),
     install_requires=['numpy',
